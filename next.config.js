1e25e579870fdb8bb3b8bbab0039534643d110d8
Patipan.m <114389220+flooksan@users.noreply.github.com>
2023-01-04 12:28:56 +0700

    Finish basic learn

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['meow.senither.com'],
+  },
+};
 
 module.exports = nextConfig
